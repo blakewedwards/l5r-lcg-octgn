388dd6ef32036241d73a999480a02a86c86ebcab
Blake Edwards <blakewedwards@gmail.com>
2017-05-03 07:07:42 -0700

    Setup provinces and stronghold

--- o8g/scripts/actions.py
+++ o8g/scripts/actions.py
@@ -1,23 +1,36 @@
-Dynasty = 'Dynasty Deck'
-Conflict = 'Conflict Deck'
-StartingHandSize = 5
+DYNASTY = 'Dynasty Deck'
+CONFLICT = 'Conflict Deck'
+STARTING_HAND_SIZE = 5
+CARD_GAP_RATIO = 1.0/10.0 # Ratio to width for space inbetween cards
 
 def setup_required(table, x, y):
   return bool(me.getGlobalVariable('setup_required'))
 
 def setup(table, x, y):
+  mute()
   if not bool(me.getGlobalVariable('setup_required')):
     notify('Set up is no longer required')
     return
   if not len(me.hand):
     notify('You must load a deck prior to setting up')
     return
-  me.hand[0].moveToTable(0, 0)
-  for card in me.hand:
-    card.moveToTable(0, 0, True)
-  me.piles[Dynasty].shuffle
-  me.piles[Conflict].shuffle
-  for card in me.piles[Conflict].top(StartingHandSize):
+  # Create the stronghold and 4 provinces in a line, with space for a row of cards above. Use the first card dimensions as reference
+  # TODO: isInverted
+  width = me.hand[0].width
+  height = me.hand[0].height
+  gap = width * CARD_GAP_RATIO
+  for i, card in enumerate(me.hand[1:]):
+    card.moveToTable(-2.5*width - 2*gap + i*(width+gap), height + 2*gap, True)
+    card.sendToBack()
+    card.peek()
+    card.anchor = True
+  stronghold = me.hand[0]
+  stronghold.moveToTable(-2.5*width - 2*gap, height + 3*gap)
+  stronghold.isFaceUp = True
+  stronghold.anchor = True
+  me.piles[DYNASTY].shuffle
+  me.piles[CONFLICT].shuffle
+  for card in me.piles[CONFLICT].top(STARTING_HAND_SIZE):
     card.moveTo(me.hand)
   notify('{} sets up'.format(me))
   me.setGlobalVariable('setup_required', '')
