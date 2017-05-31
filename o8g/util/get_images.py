9cc0763c1ef8269649cbf6c3da051f86092fa220
Blake Edwards <blakewedwards@gmail.com>
2017-05-31 05:03:41 -0700

    Parse set file and query images

--- o8g/util/get_images.py
+++ o8g/util/get_images.py
@@ -1,5 +1,12 @@
 import urllib2
 from HTMLParser import HTMLParser  
+from xml.dom import minidom
+
+set = 'set.xml'
+
+dom = minidom.parse(set)
+cards = [(card.getAttribute('name'), card.getAttribute('id')) for card in dom.getElementsByTagName('card')]
+print cards
 
 class MyHTMLParser(HTMLParser):
 
@@ -28,15 +35,25 @@ class MyHTMLParser(HTMLParser):
   def handle_endtag(self, tag):
     if tag == 'div':
       self.file = False
-      
-name = 'Above Question'
-req = urllib2.Request(url='https://l5r.gamepedia.com/File:' + name.replace(' ', '_') + '.png', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'})
-f = urllib2.urlopen(req)
-p = MyHTMLParser(name)
-html = f.read()
-p.feed(html)
-for image_url in p.image_urls:
-  with open(name + '.png', 'wb') as f:
-    print image_url
-    f.write(urllib2.urlopen(image_url).read())
-p.close()
+
+missing = []
+for (name, id) in cards:
+  if name == 'Yojin no Shiro':
+    name = 'Shiro no Yojin'
+  url = 'http://l5r.gamepedia.com/File:' + name.replace(' ', '_') + '.png'
+  print '[' + name + '] Getting ' + url + '...'
+  try:
+    req = urllib2.Request(url=url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'})
+    f = urllib2.urlopen(req)
+    p = MyHTMLParser(name)
+    html = f.read()
+    p.feed(html)
+    for image_url in p.image_urls:
+      with open('images/' + id + '.png', 'wb') as f:
+        print image_url
+        f.write(urllib2.urlopen(image_url).read())
+    p.close()
+  except urllib2.HTTPError as err:
+    missing.append(name)
+
+print 'Missing: ' + str(missing)
