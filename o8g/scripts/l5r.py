ad5f8f8946a4ea9c9c33f5f7747630edf2b9ad01
Blake Edwards <blakewedwards@gmail.com>
2017-09-04 05:57:19 -0700

    Adjust messages and add missing ok=False for deck validation

--- o8g/scripts/l5r.py
+++ o8g/scripts/l5r.py
@@ -1,6 +1,5 @@
 def validate_deck():
   mute()
-  notify (" -> Checking deck of {} ...".format(me))
   ok = True
   StrongholdCount = 0
   RoleCount = 0
@@ -20,34 +19,35 @@ def validate_deck():
       RoleName = card.Name
     elif card.type != TYPE_PROVINCE:
       ok = False
-      whisper("You have a card in your hand that is not a stronghold, role or province, please put it in the appropriate deck.")
+      whisper('Only strongholds, roles, and provinces should be in hand.')
 
   if StrongholdCount != 1:
     ok = False
-    whisper("You should have exactly 1 stronghold card in your hand.")
+    whisper('Exactly 1 stronghold should be in hand.')
+
   if RoleCount > 1:
     ok = False
-    whisper("You can only use 1 role.")
+    whisper('At most 1 role should be in hand.')
 
-  #Provinces
+  # Provinces
   nameCounts = collections.defaultdict(int)
   ringCounts = collections.defaultdict(int)
   provinces = [c for c in me.hand if c.type == TYPE_PROVINCE]
 
   if len(provinces) != MAX_PROVINCES:
     ok = False
-    whisper("You must have exactly 5 provinces.")
+    whisper('Exactly 5 provinces should be in hand.')
 
   for card in provinces:
-    if card.Clan != 'Neutral' and ClanName not in card.Clan:
+    if card.Clan != 'Neutral' and ClanName != card.Clan:
       ok = False
-      notify("{}'s provinces contain a card from another clan. Name is {}.".format(me,card))
+      whisper('Provinces must be in-clan or neutral, {} is not.'.format(card))
 
     nameCounts[card.name] += 1
 
     if nameCounts[card.name] > 1:
       ok = False
-      notify("{} has {} copies of {}, but can have only 1.".format(me, nameCounts[card.name], card))
+      whisper('At most 1 copy of a province may be used, {} has {}.'.format(card, nameCounts[card.name]))
 
     ringCounts[card.Ring] += 1
 
@@ -58,81 +58,77 @@ def validate_deck():
 
     if ringCounts[card.Ring] > limit:
       ok = False
-      notify("{} has {} {} provinces, but can have only {}".format(me, ringCounts[card.Ring], card.Ring, limit))
+      whisper('At most {} {} province(s) may be used.'.format(limit, card.Ring))
 
-  #Dynasty deck
+  # Dynasty deck
   me.piles[DYNASTY].addViewer(me)
 
-  if len(me.piles[DYNASTY]) < 40:
+  if len(me.piles[DYNASTY]) < 40 or len(me.piles[DYNASTY]) > 45:
     ok = False
-    notify("{}'s dynasty deck must contain at least 40 cards".format(me))
-  if len(me.piles[DYNASTY]) > 45:
-    ok = False
-    notify("{}'s dynasty deck must contain at most 45 cards".format(me))
+    whisper('A dynasty deck must contain a minimum of 40 cards and a maximum of 45.')
 
   for card in me.piles[DYNASTY]:
-    if card.type != TYPE_CHARACTER and card.type != TYPE_HOLDING:
+    if card.deck != 'Dynasty':
       ok = False
-      notify("{}'s dynasty deck must contain only characters and holdings. Name is {}.".format(me,card))
+      whisper('A dynasty deck must contain only dynasty cards, {} is not one.'.format(card))
 
-    if card.Clan != 'Neutral' and ClanName not in card.Clan:
-      notify("{}'s dynasty deck contain a card from another clan. Name is {}.".format(me,card))
+    if card.Clan != 'Neutral' and ClanName != card.Clan:
+      ok = False
+      whisper('A dynasty deck must contain in-clan or neutral cards, {} is not one.'.format(card))
 
     if card.name == 'Seeker Initiate' and not RoleName.startswith('Seeker'):
       ok = False
-      notify("Seeker Initiate requires a Seeker role.")
+      whisper('Seeker Initiate may only be used with a Seeker role.')
 
     if card.name == 'Keeper Initiate' and not RoleName.startswith('Keeper'):
       ok = False
-      notify("Keeper Initiate requires a Keeper role.")
+      whisper('Keeper Initiate may only be used with a Keeper role.')
 
     nameCounts[card.name] += 1
 
     if nameCounts[card.name] > 3:
       ok = False
-      notify("{} has {} copies of {}, but can have only 3.".format(me, nameCounts[card.name], card))
+      whisper('At most 3 copies of a dynasty card may be used, {} has {}.'.format(card, nameCounts[card.name]))
 
   me.piles[DYNASTY].removeViewer(me)
 
-  #Conflict deck
+  # Conflict deck
   me.piles[CONFLICT].addViewer(me)
 
-  if len(me.piles[CONFLICT]) < 40:
+  if len(me.piles[CONFLICT]) < 40 or len(me.piles[CONFLICT]) > 45:
     ok = False
-    notify("{}'s conflict deck must contain at least 40 cards".format(me))
-  if len(me.piles[CONFLICT]) > 45:
-    ok = False
-    notify("{}'s conflict deck must contain at most 45 cards".format(me))
+    whisper('A conflict deck must contain a minimum of 40 cards and a maximum of 45.')
 
   for card in me.piles[CONFLICT]:
-    if card.type != TYPE_CHARACTER and card.type != TYPE_ATTACHMENT and card.type != TYPE_EVENT:
+    if card.deck != 'Conflict':
       ok = False
-      notify("{}'s conflict deck must contain only characters, attachments and events. Name is {}.".format(me,card))
+      whisper('A conflict deck must contain only conflict cards, {} is not one.'.format(card))
 
     if card.type == TYPE_CHARACTER:
       ConflictCharCount += 1
 
-    if card.Clan != 'Neutral' and ClanName not in card.Clan:
+    if card.Clan != 'Neutral' and ClanName != card.Clan:
       if card.properties[INFLUENCE_VALUE] == '':
         ok = False
-        notify("{}'s conflict deck contains an out-of-clan card with no influence cost. Name is {}.".format(me,card))
+        whisper('Out-of-clan conflict cards must have an influence cost, {} does not.'.format(card))
       else:
         InfluenceCount += int(card.properties[INFLUENCE_VALUE])
 
       if InfluenceClanName == '':
         InfluenceClanName = card.Clan
-      elif InfluenceClanName not in card.Clan:
-        notify("{}'s conflict deck contains out-of-clan cards from more than 1 clan.".format(me))
+      elif InfluenceClanName != card.Clan:
+        ok = False
+        whisper('At most 1 clan may be chosen for out-of-clan conflict cards.')
 
     nameCounts[card.name] += 1
 
     if nameCounts[card.name] > 3:
       ok = False
-      notify("{} has {} copies of {}, but can have only 3.".format(me, nameCounts[card.name], card))
+      whisper('At most 3 copies of a conflict card may be used, {} has {}.'.format(card, nameCounts[card.name]))
 
-    if ConflictCharCount > 10:
-     ok = False
-     notify("{}'s conflict deck contains {} character, but can have only 10".format(me, ConflictCharCount))
+  if ConflictCharCount > 10:
+    ok = False
+    whisper('At most 10 characters may be included in a conflict deck, found {}.'.format(ConflictCharCount))
 
   if RoleName.startswith('Keeper'):
     InfluenceLimit = 13
@@ -141,10 +137,11 @@ def validate_deck():
 
   if InfluenceCount > InfluenceLimit:
     ok = False
-    notify("{}'s conflict deck contains out-of-clan cards with {} influence, but can have only {}".format(me, InfluenceCount, InfluenceLimit))
+    whisper('At most {} influence of out-of-clan cards may be included in a conflict deck, found {}.'.format(InfluenceLimit, InfluenceCount))
+
   me.piles[CONFLICT].removeViewer(me)
 
   if ok:
-    notify("Deck of {} is OK".format(me))
+    notify("{}'s deck is OK.".format(me))
   else:
-    notify("Deck of {} is NOT OK".format(me))
+    notify("{}'s deck is NOT OK.".format(me))
